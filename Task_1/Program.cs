0b74977279c5fe9da5004ca7a2226418479b6e5a
«Kristina» <kristinabol1996@gmail.com>
2022-09-03 08:53:11 +0800

    1 задача

---
+++ Task_1/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задайте двумерный массив. Напишите программу, которая поменяет местами первую и последнюю строку массива.
+
+int[,] CreateMatrix(int rowCount, int columnCount, int min, int max)
+{
+    int[,] array = new int[rowCount, columnCount];
+    for (int row = 0; row < rowCount; row++)
+    {
+        for (int column = 0; column < columnCount; column++)
+        {
+            array[row, column] = new Random().Next(min, max + 1);
+        }
+    }
+    return array;
+}
+
+void PrintMatrix(int[,] array)
+{
+    for (int row = 0; row < array.GetLength(0); row++)
+    {
+        for (int column = 0; column < array.GetLength(1); column++)
+        {
+            Console.Write($"{array[row, column]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void ReverseRows(int[,] array)
+{
+    int rowCount = array.GetLength(0);
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        // array[0, i] <-> array [rowcount - 1, i]
+        int temp = array[0,i];
+        array[0,i] = array[rowCount - 1, i];
+        array[rowCount -1, i] = temp;
+    }
+}
+
+int[,] matrix = CreateMatrix(6, 8, 0, 9);
+PrintMatrix(matrix);
+Console.WriteLine();
+ReverseRows(matrix);
+PrintMatrix(matrix);

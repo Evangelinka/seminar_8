de4d746a3047f9b590ee9b57080eee509a17b8be
«Kristina» <kristinabol1996@gmail.com>
2022-09-03 08:54:11 +0800

    2 задача

---
+++ Task_2/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задайте двумерный массив. Напишите программу, которая заменяет строки на столбцы. 
+// В случае, если это невозможно, программа должна вывести сообщение для пользователя.
+
+int[,] CreateMatrix(int row, int column, int min, int max)
+{
+    int[,] matrix = new int[row, column];
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            matrix[i, j] = new Random().Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] TranspositionMatrix(int[,] array)
+{
+    int[,] matrix = new int[array.GetLength(1), array.GetLength(0)];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            matrix[j, i] = array[i, j];
+        }
+    }
+    return matrix;
+}
+
+int[,] array = CreateMatrix(1, 4, 0, 9);
+PrintMatrix(array);
+Console.WriteLine();
+int[,] transMatrix = TranspositionMatrix(array);
+PrintMatrix(transMatrix);
